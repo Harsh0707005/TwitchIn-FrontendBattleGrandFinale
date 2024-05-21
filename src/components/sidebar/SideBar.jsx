e079c66ce4b1af426c3805a9c63e897051127db2
Harsh0707005 <harshmaster07705@gmail.com>
2024-05-21 23:45:07 +0530

    Commit

---
+++ src/components/sidebar/SideBar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './style.css'
+
+const SideBar = () => {
+  return (
+    <div className='bg-[rgb(31,31,35)] text-white w-[20%]'>SideBar</div>
+  )
+}
+
+export default SideBar
