e63f035a8855fafb3d7733108e53a724b0741891
Harsh0707005 <harshmaster07705@gmail.com>
2024-05-22 08:10:43 +0530

    search routing

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, createBrowserRouter, Router, RouterProvider } from 'react-router-dom'
+import SideBar from './components/sidebar/SideBar.jsx'
+import SearchResult from './components/search/SearchResult.jsx'
+import Navbar from './components/navbar/Navbar.jsx'
+import ProgressBar from './components/progressbar/ProgressBar.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <> <Navbar /> <div className='flex flex-row'> <SideBar /> <SearchResult /> </div> </>
+  },
+  {
+    path: "/search",
+    element: <> <ProgressBar/> <Navbar /> <div className='flex flex-row'> <SideBar /> <SearchResult /> </div> </>
+  },
+])
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <RouterProvider router={router} />
 )
